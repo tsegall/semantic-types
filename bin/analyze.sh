76e1a3e627ce6c5f9647369e7b99cbecb895d3ed
Tim Segall <xtim@cobber.com>
2022-11-28 13:59:02 -0800

    Large number of address related fixups

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -1,2 +1,24 @@
-find data -name '*.csv'  -print | xargs -n 1 -P 8 bin/doit.sh
-bin/current.sh
+diff ncurrent.csv current.csv | egrep '^<' | sed 's/^..//' | cut -f1,2 -d',' | sed 's/$/,/' > /tmp/$$.1
+echo "Differences: $(wc -l < /tmp/$$.1)"
+for i in `cat /tmp/$$.1`
+do
+	F=`egrep $i reference.csv | cut -f5 -d','`
+	R=`egrep $i reference.csv | cut -f8 -d','`
+	N=`egrep $i ncurrent.csv | cut -f8 -d','`
+	C=`egrep $i current.csv | cut -f8 -d','`
+
+	if [ "$R" != "$N" ]
+	then
+		echo "---- $i, field $F, R = $R, N = $N"
+	else
+		echo "++++ $i, field $F, R = $R, N = $N, C = $C"
+	fi
+done
+
+bin/performance | tail -1
+cp current.csv /tmp
+cp ncurrent.csv current.csv
+bin/performance | tail -1
+cp /tmp/current.csv .
+
+
--- bin/doit.sh
+++ bin/doit.sh
@@ -20,6 +20,12 @@ do
 		exit 0
 	fi
 
+        # See if there is an options file and if so add them to the options
+        if [ -f "$1.options" ]
+        then
+                OPTIONS="$OPTIONS $(cat "$1".options)"
+        fi
+
 	$FTA $OPTIONS --json --output --xMaxCharsPerColumn 20000 --debug 1 --records 1000 "$i"
 done
 
